e936c36fdc1aab456016c8a42fb6f0500a76ee84
JacobMJones <mrjacobtaiwa@gmail.com>
2018-06-26 16:21:59 -0400

    prepareDataSet function parses text based datasets

--- naturalbraintest.js
+++ naturalbraintest.js
@@ -1,14 +1,7 @@
 var BrainJSClassifier = require("natural-brain");
 var classifier = new BrainJSClassifier();
+const fs = require('fs');
 
-// classifier.addDocument('my unit-tests failed.', 'software');
-// classifier.addDocument('tried the program, but it was buggy.', 'software');
-// classifier.addDocument('tomorrow we will do standup.', 'meeting');
-// classifier.addDocument('the drive has a 2TB capacity.', 'hardware');
-// classifier.addDocument('i need a new power supply.', 'hardware');
-// classifier.addDocument('can you play some new music?', 'music');
-
-// saves classifier to filename
 const saveClassifier = filename => {
     classifier.save(filename, function(err, classifier) {});
 };
@@ -21,30 +14,11 @@ const loadClassifier = (filename, cb) => {
 const loadedCB = (error, classifier) => {
     console.log("loaded");
     console.log(classifier.classify("did the tests pass?")); // -> software
-    console.log(classifier.classify("did you buy a new drive?")); // -> hardware
-    console.log(classifier.classify("What is the capacity?")); // -> hardware
-    console.log(classifier.classify("Lets meet tomorrow?")); // -> meeting
-    console.log(classifier.classify("Can you play some stuff?")); // -> music
 };
 
-// console.log("loaded");
-// classifier.addDocument("i love pie", "food");
-// console.log("new feature added");
-// classifier.retrain();
-// classifier.save("classifier.json", function(err, classifier) {});
 
-// BrainJSClassifier.load("classifier.json", null, null, function(error, classifier) {
-//     console.log("loaded");
-//   console.log(classifier.classify("did the tests pass?")); // -> software
-//   console.log(classifier.classify("did you buy a new drive?")); // -> hardware
-//   console.log(classifier.classify("What is the capacity?")); // -> hardware
-//   console.log(classifier.classify("Lets meet tomorrow?")); // -> meeting
-//   console.log(classifier.classify("Can you play some stuff?")); // -> music
-//   console.log(classifier.classify('pie is great')); // -> personal if worked, software if failed
-// })
-
-// take in an array of arrays of strings [['input','output'], ['input','output']]
 const createNewClassifier = () => {
+
     classifier.addDocument("my unit-tests failed.", "software");
     classifier.addDocument("tried the program, but it was buggy.", "software");
     classifier.addDocument("tomorrow we will do standup.", "meeting");
@@ -55,17 +29,30 @@ const createNewClassifier = () => {
     saveClassifier("classifier.json");
 };
 
+const prepareDataSet = (dataSet) => {
+    console.log('in prepareDataSet');
+    var parsedArray = fs.readFileSync(dataSet).toString().split("\n");;
+    finalArray = [];
+    for (var i = 0; i < parsedArray.length - 1; i++) {
+        let dataPoint = parsedArray[i];
+        let match = dataPoint.match(/(.*)\s(0|1)$/)
+        dataObject = {};
+        dataObject.content = match[1];
+        match[2] == 1 ? dataObject.sentiment = 'positive' : dataObject.sentiment = 'negative';
+        finalArray.push(dataObject);
+    }
+    console.log(finalArray)
 
-loadClassifier('classifier.json', loadedCB)
-
+}
 const createEmptyJson = (name) => {
     typeof name === 'string' ? fs.writeFileSync(`${name}.json`, "file contents") : console.log('create file failed')
 }
 
-// createNewClassifier()
+prepareDataSet('yelp.txt');
 
-// console.log(classifier.classify('did the tests pass?')); // -> software
-// console.log(classifier.classify('did you buy a new drive?')); // -> hardware
-// console.log(classifier.classify('What is the capacity?')); // -> hardware
-// console.log(classifier.classify('Lets meet tomorrow?')); // -> meeting
-// console.log(classifier.classify('Can you play some stuff?')); // -> music
+// createNewClassifier();
+// loadClassifier('classifier.json', loadedCB)
+
+
+
+// createEmptyJson('test')
