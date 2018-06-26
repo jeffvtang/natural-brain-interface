ec0dea88c63b27c323859c5620d961391819ef06
jeffvtang <jeff.tang@gmail.com>
2018-06-26 23:25:07 +0000

    fixed retrain

--- natural.js
+++ natural.js
@@ -2,7 +2,8 @@
 // var classifier = new BrainJSClassifier();
 
 var natural = require("natural");
-var classifier = new natural.BayesClassifier();
+// var classifier = new natural.BayesClassifier();
+// var classifier = new natural.Classifier();
 
 const createEmptyJson = name => {
   typeof name === "string"
@@ -11,8 +12,8 @@ const createEmptyJson = name => {
 };
 
 // saves classifier to filename
-const saveClassifier = filename => {
-  classifier.save(filename, function(err, classifier) {});
+const saveClassifier = (input, filename) => {
+  input.save(filename, function(err, classifier) {});
 };
 
 // save - promise version
@@ -29,12 +30,21 @@ const saveClassifier = filename => {
 // };
 
 // loads classifier from filename, then does callback
+// const loadClassifier = (filename, cb) => {
+//   natural.BayesClassifier.load(filename, null, cb);
+// };
+// const loadClassifier = (filename, cb) => {
+//   natural.BayesClassifier.load(filename, null, cb);
+// };
+
 const loadClassifier = (filename, cb) => {
   natural.BayesClassifier.load(filename, null, cb);
 };
 
-const loadedCB = (error, classifier) => {
+const loadedCB = classifier => {
+// const loadedCB = (error, classifier) => {
   console.log("loaded database");
+  // console.log(classifier)
   console.log(classifier.classify("did the tests pass?")); // -> software
   console.log(classifier.classify("did you buy a new drive?")); // -> hardware
   console.log(classifier.classify("What is the capacity?")); // -> hardware
@@ -48,26 +58,37 @@ const retrainClassifier = (error, classifier) => {
   classifier.addDocument("i love pie", "food");
   console.log("new feature added");
   classifier.retrain();
-  saveClassifier("classifier.json");
+  saveClassifier(classifier, 'classifier.json')
 };
 
 // take in an array of arrays of strings [['input','output'], ['input','output']]
 const createNewClassifier = () => {
+  var classifier = new natural.BayesClassifier();
   classifier.addDocument("my unit-tests failed.", "software");
   classifier.addDocument("tried the program, but it was buggy.", "software");
   classifier.addDocument("tomorrow we will do standup.", "meeting");
   classifier.addDocument("the drive has a 2TB capacity.", "hardware");
   classifier.addDocument("i need a new power supply.", "hardware");
   classifier.addDocument("can you play some new music?", "music");
-  classifier.addDocument("pie is my greatest love", "food");
+  // classifier.addDocument("pie is my greatest love", "food");
   classifier.train();
-  console.log("classifier trained");
-  saveClassifier("classifier.json");
-  console.log("classifier saved");
+  // console.log("classifier trained");
+  saveClassifier(classifier, "classifier.json");
+  // return classifier;
+  // console.log("classifier saved");
 };
 
-// createNewClassifier();
-loadClassifier('classifier.json', loadedCB)
+// const classifier = createNewClassifier();
+// classifier.addDocument();
+
+// createNewClassifier()
+// loadClassifier('classifier.json', loadedCB)
+// loadClassifier('classifier.json', retrainClassifier)
+// loadClassifier('classifier.json', loadedCB)
+loadClassifier('classifier.json', function(err, x) {
+  console.log(x)
+})
+// loadedCB(classifier)
 
 // const createNewClassifier = () => {
 //   return new Promise((resolve, reject) => {
@@ -106,12 +127,13 @@ loadClassifier('classifier.json', loadedCB)
 // classifier.addDocument("i need a new power supply.", "hardware");
 // classifier.addDocument("can you play some new music?", "music");
 // classifier.addDocument("pie is my greatest love", "food");
-
 // classifier.train();
 
-// console.log(classifier.getClassifications("did the tests pass?")); // -> software
-// console.log(classifier.getClassifications("did you buy a new drive?")); // -> hardware
-// console.log(classifier.getClassifications("What is the capacity?")); // -> hardware
-// console.log(classifier.getClassifications("Lets meet tomorrow?")); // -> meeting
-// console.log(classifier.getClassifications("Can you play some stuff?")); // -> music
-// console.log(classifier.getClassifications("pie is the greatest")); // -> software if fail
+// // console.log(classifier)
+
+// console.log(classifier.classify("did the tests pass?")); // -> software
+// console.log(classifier.classify("did you buy a new drive?")); // -> hardware
+// console.log(classifier.classify("What is the capacity?")); // -> hardware
+// console.log(classifier.classify("Lets meet tomorrow?")); // -> meeting
+// console.log(classifier.classify("Can you play some stuff?")); // -> music
+// console.log(classifier.classify("pie is the greatest")); // -> software if fail
